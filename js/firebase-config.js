93584366b1e6025680fd400551fabf4c0a8c94f9
tjdelapaz424-svg <tjdelapaz424@gmail.com>
2026-09-13 04:07:45 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDbRELdDR5e_-I7X9Tp8ekQEjCzGuAyU0",
+  apiKey: "AIzaSyDbREL6DR5e_-_I7X9Tp8ekQEjCzGuAyU0",
   authDomain: "notesonline-c3f32.firebaseapp.com",
+  databaseURL: "https://notesonline-c3f32-default-rtdb.firebaseio.com",
   projectId: "notesonline-c3f32",
   storageBucket: "notesonline-c3f32.firebasestorage.app",
   messagingSenderId: "1083062612326",
-  appId: "1:1083062612326:web:d1ef37eab0bd42307358a7"
+  appId: "1:1083062612326:web:d1ef37eab0bd42307358a7",
+  measurementId: "G-PJ6T1KXSEN"
 };
 
 firebase.initializeApp(firebaseConfig);
